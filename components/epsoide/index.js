c3f48b4189fb8803057e1b42382815f2ec5983be
xp <work_xp@126.com>
2018-11-27 01:20:53 +0800

    头部

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -1,16 +1,32 @@
 Component({
   properties: {
-    index: Number
+    index: {
+      type: Number,
+      observer: function (newVal, oldVal, changePath) {
+        let val = newVal < 10?'0'+newVal:newVal
+        this.setData({
+          _index: val
+        })
+      }
+    }
   },
   data: {
+    months: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
     year: 0,
-    month: ''
+    month: '',
+    _index: ''
+  },
+  attached: function () {
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+    
+    this.setData({
+      year: year,
+      month: this.data.months[month]
+    })
   },
   methods: {
 
-  },
-  attached(){
-    console.log(this.data);
-    
   }
 })
