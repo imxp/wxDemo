035dfbc2ec9577578e212226c2b7d0d0b1bde9f4
xp <work_xp@126.com>
2018-12-01 17:49:18 +0800

    搜索历史记录 热门搜索

--- components/search/index.js
+++ components/search/index.js
@@ -1,15 +1,40 @@
+import { BookModel } from '../../modules/book.js'
+import { KeywordModel } from '../../modules/keyword.js'
+
+
+const bookModel = new BookModel()
+const keywordModel = new KeywordModel()
+
 Component({
   properties: {
 
   },
 
   data: {
+    historyWords: [],
+    hotWords: []
+  },
 
+  attached() {
+    
+    this.setData({
+      historyWords: keywordModel.getHistory()
+    })
+
+    keywordModel.getHot().then((res) => {
+      this.setData({
+        hotWords: res.hot
+      })
+    })
   },
 
   methods: {
     onCancel: function (event) {
       this.triggerEvent('cancel',{},{})
+    },
+    onConfirm: function (event) {
+      const word = event.detail.value
+      keywordModel.addToHistory(word)
     }
   }
 })
